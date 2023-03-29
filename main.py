ea9ecfd2bc67d257540121b09271cab088d0c53d
Dmitriy Smirnov <git@d-sm.ru>
2023-03-29 21:41:30 +0300

    task3 completed

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from datetime import datetime
+
 from application.salary import calculate_salary
 from application.db.people import get_employees
 
+def print_func_exec_start_time(func):
+  print(f'Function "{func.__name__}" execution start time: {datetime.now()}')
+  func()
+
 if __name__ == '__main__':
-  calculate_salary()
-  get_employees()
+  print_func_exec_start_time(calculate_salary)
+  print_func_exec_start_time(get_employees)
